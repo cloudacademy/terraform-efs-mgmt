5d784d1102616005e10a71d8f7a445a389eec165
Jeremy Cook <jeremy.cook@cloudacademy.com>
2024-05-27 12:01:19 +1200

    added randomization to kms alias

--- terraform/modules/kms/main.tf
+++ terraform/modules/kms/main.tf
@@ -1,3 +1,9 @@
+resource "random_string" "kms" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
 resource "aws_kms_key" "key" {
   description             = var.kms_key_description
   key_usage               = "ENCRYPT_DECRYPT"
@@ -5,6 +11,6 @@ resource "aws_kms_key" "key" {
 }
 
 resource "aws_kms_alias" "alias" {
-  name          = "alias/${var.kms_key_alias}"
+  name          = "alias/${var.kms_key_alias}-${random_string.kms.result}"
   target_key_id = aws_kms_key.key.key_id
 }
