797eba366c9b8a933bf1236cd36df3dbb97a3e4e
Jeremy Cook <jeremy.cook@cloudacademy.com>
2024-05-31 12:13:35 +1200

    added extra output

--- terraform/modules/efs/outputs.tf
+++ terraform/modules/efs/outputs.tf
@@ -5,3 +5,7 @@ output "file_system_id" {
 output "file_system_fqdn" {
   value = aws_efs_file_system.file_system.dns_name
 }
+
+output "customer_managed_security_group_id" {
+  value = aws_security_group.customer_managed.id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "efs_filesystem_id" {
 output "efs_filesystem_fqdn" {
   value = module.efs.file_system_fqdn
 }
+
+output "customer_managed_security_group_id" {
+  value = module.efs.customer_managed_security_group_id
+}
